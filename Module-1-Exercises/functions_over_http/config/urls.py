488c21afb17f5ef7b388650a9c330cfcfaa2eb5e
Blair <blairjohnson562@gmail.com>
2023-10-26 16:04:56 -0500

    Finished

--- Module-1-Exercises/functions_over_http/config/urls.py
+++ Module-1-Exercises/functions_over_http/config/urls.py
@@ -1,7 +1,8 @@
-"""config URL Configuration
+"""
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import hello_view, age_view, order_total
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hey/<name>/', hello_view),
+    path('age-in/<int:end>/<int:birthyear>/', age_view),
+    path('order-total/<int:burgers>/<int:fries>/<int:drinks>/', order_total)
 ]
